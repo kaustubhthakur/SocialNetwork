33c28260abaeabd5ae3269aa2d4f57caf6ab07f3
Kaustubh <kaustubhthakur76@gmail.com>
2024-11-26 22:28:19 +0530

    cant code frontend 0%

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,14 @@ const port = 8000
 const cors = require('cors')
 const mongoose = require('mongoose')
 const cookieparser = require('cookie-parser')
+const bodyParser = require('body-parser')
 require('dotenv').config()
 const postrouter = require('./routes/posts')
 const userrouter = require('./routes/users')
 const authrouter = require('./routes/auth')
-app.use(express.json());
+app.use(express.json({ limit: "5mb" })); // to parse req.body
+// limit shouldn't be too high to prevent DOS
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 app.use(cors());
 app.use(cookieparser());
 
