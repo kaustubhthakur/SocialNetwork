d9f387441a89c46b2994eeb487742e9250654c9f
Kaustubh <kaustubhthakur76@gmail.com>
2024-12-01 22:09:34 +0530

    backend 2%

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
 const express = require('express')
+const app = express();
+const port = 9000;
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+
+app.use(express.json())
+app.use(cors())
+
+const connection = async()=> {
+    try {
+        await mongoose.connect(process.env.MONGODB);
+        console.log('database is connected...')
+    } catch (error) {
+        console.error(error);
+    }
+}
+connection();
+app.listen(port,()=> {
+    console.log(`server is connected to ${port}...`)
+})
